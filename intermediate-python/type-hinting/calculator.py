b36d673460c8ec078dac7e7c9ee03e00dc0f8fc4
Mark Chesney <mark.chesney@gmail.com>
2018-11-13 17:39:02 -0800

    add calculator.py, to demonstrate type hinting

--- intermediate-python/type-hinting/calculator.py
+++ intermediate-python/type-hinting/calculator.py
@@ -1,18 +1,26 @@
-def add(addend1, addend2):
+def add(addend1: int, addend2: int) -> int:
     return addend1 + addend2
 
 
-def subtract(minuend, subtrahend):
+def subtract(minuend: int, subtrahend: int) -> int:
     return minuend - subtrahend
 
 
-def multiply(factor1, factor2):
+def multiply(factor1: int, factor2: int) -> int:
     return factor1 * factor2
 
 
-def divide(numerator, denominator):
+def divide(numerator: int, denominator: int) -> float:
     return numerator / denominator
 
 print(add(5, 8.2))
 print(multiply('hello ', 5))
 
+
+# Real and Imaginary numbers
+# def add_complex_numbers(addend1: complex, addend2: complex) -> complex:
+#     return addend1 + addend2
+#
+# from numbers import Real
+# def add_real_numbers(addend1: Real, addend2: Real) -> complex:
+#     return addend1 + addend2
