279bd4f931740198f5969f5e2ebe0cab45e1b2d6
Mark Chesney <mark.chesney@gmail.com>
2018-11-19 17:44:49 -0800

    yield update

---
+++ intermediate-python/generators/yield.py
@@ -0,0 +1,3 @@
+# yield
+# A Python generator is a function which returns a generator iterator (just an object we can iterate over) by calling yield. yield may be called with a value, in which case that value is treated as the "generated" value.
+
