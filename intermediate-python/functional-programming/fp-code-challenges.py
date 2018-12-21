ce1b26759aed2269cdb64c779ccaf836b4e82d79
Mark Chesney <mark.chesney@gmail.com>
2018-12-21 12:03:48 -0800

    add attrgetter to fp

--- intermediate-python/functional-programming/fp-code-challenges.py
+++ intermediate-python/functional-programming/fp-code-challenges.py
@@ -1,5 +1,7 @@
 # Code challenges
 
+# itemgetter
+
 from operator import itemgetter
 
 fruit_list = [
@@ -11,3 +13,25 @@ fruit_list = [
 ]
 
 sorted_fruit = sorted(fruit_list, key=itemgetter(1))
+
+# attrgetter
+
+import datetime
+from operator import attrgetter
+
+# Use sorted() and attrgetter() to sort date_list
+# by the day attribute of each datetime object.
+# Save this sorting into a variable named sorted_dates.
+
+date_list = [
+    datetime.datetime(2015, 4, 29, 10, 15, 39),
+    datetime.datetime(2006, 8, 15, 14, 59, 2),
+    datetime.datetime(1981, 5, 16, 2, 10, 42),
+    datetime.datetime(2012, 8, 9, 14, 59, 2),
+]
+
+# Use sorted() and attrgetter() to sort date_list by the day attribute of each
+# datetime object.
+
+# Save this sorting into a variable named sorted_dates.
+sorted_dates = sorted(date_list, key=attrgetter('day'))
