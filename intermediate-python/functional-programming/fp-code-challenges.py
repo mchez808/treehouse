0911d86813a6a631015118dad55fe8b8c5b4bb72
Mark Chesney <mark.chesney@gmail.com>
2018-12-29 15:31:42 -0800

    complete unit 2 of FP: Functional Workhorses

--- intermediate-python/functional-programming/fp-code-challenges.py
+++ intermediate-python/functional-programming/fp-code-challenges.py
@@ -35,3 +35,30 @@ date_list = [
 
 # Save this sorting into a variable named sorted_dates.
 sorted_dates = sorted(date_list, key=attrgetter('day'))
+
+
+# Using c_to_f and a list comprehension, create a variable named good_temps.
+# Convert each Celsius temperature into Fahrenheit,
+# but only if the Celsius temperature is between 9 and 32.6.
+
+temperatures = [
+    37,
+    0,
+    25,
+    100,
+    13.2,
+    29.9,
+    18.6,
+    32.8
+]
+
+
+def c_to_f(temp):
+    """Returns Celsius temperature as Fahrenheit"""
+    return temp * (9/5) + 32
+
+def is_in_domain(temp_celsius):
+    """but only if the Celsius temperature is between 9 and 32.6"""
+    return 9 <= temp_celsius and temp_celsius <= 32.6
+
+good_temps = [c_to_f(temp) for temp in temperatures if is_in_domain(temp)]
