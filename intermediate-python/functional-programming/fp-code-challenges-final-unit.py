9ac00173802a3e64ac1e14e732713d6a9e7b0352
Mark Chesney <mark.chesney@gmail.com>
2019-01-04 19:49:40 -0800

    complete recursion (courses and prereqs challenge)

--- intermediate-python/functional-programming/fp-code-challenges-final-unit.py
+++ intermediate-python/functional-programming/fp-code-challenges-final-unit.py
@@ -1,3 +1,34 @@
+
+#########
+from functools import reduce
+from operator import add
+
+prices = [
+    (6.99, 5),
+    (2.94, 15),
+    (156.99, 2),
+    (99.99, 4),
+    (1.82, 102)
+]
+
+# Let's start by writing a function named product_sales that takes a single two-member tuple made up of a price and a number of units sold. product_sales should return the product of the price and the number of units.
+
+def product_sales(tuple_value):
+    price, units = tuple_value
+    return price * units
+
+print(product_sales((10.5, 2)))
+
+intermediate = map(product_sales, prices)
+total = reduce(add, intermediate)
+# Use map() to find the per-product totals for each item in prices,
+# then use reduce (and add) to find the total value.
+
+if total != (6.99*5) + (2.94*15) + (156.99*2) + (99.99*4) + (1.82*102):
+    raise ValueError("sum is incorrect")
+#########
+
+
 # Finish the prereqs function so that it recursively finds all of the prerequisite
 # course titles in courses (like "Object-Oriented Python" is a prerequisite for
 # "Django Basics"). You should add() the title of the prerequisite to the pres set
@@ -27,7 +58,6 @@ courses = {'count': 2,
 def prereqs(data, pres=None):
     pres = pres or set()
     list_of_dicts = data['prereqs']
-    import pdb; pdb.set_trace()
     if list_of_dicts:
         for dict in list_of_dicts:
             pres.add(dict['title'])
@@ -43,3 +73,5 @@ required_courses = prereqs(courses)
 
 # incorrect contents
 {'Django Basics'}
+
+####################
--- intermediate-python/functional-programming/fp-code-challenges.py
+++ intermediate-python/functional-programming/fp-code-challenges.py
@@ -62,33 +62,3 @@ def is_in_domain(temp_celsius):
     return 9 <= temp_celsius and temp_celsius <= 32.6
 
 good_temps = [c_to_f(temp) for temp in temperatures if is_in_domain(temp)]
-
-
-#########
-from functools import reduce
-from operator import add
-
-prices = [
-    (6.99, 5),
-    (2.94, 15),
-    (156.99, 2),
-    (99.99, 4),
-    (1.82, 102)
-]
-
-# Let's start by writing a function named product_sales that takes a single two-member tuple made up of a price and a number of units sold. product_sales should return the product of the price and the number of units.
-
-def product_sales(tuple_value):
-    price, units = tuple_value
-    return price * units
-
-print(product_sales((10.5, 2)))
-
-intermediate = map(product_sales, prices)
-total = reduce(add, intermediate)
-# Use map() to find the per-product totals for each item in prices,
-# then use reduce (and add) to find the total value.
-
-if total != (6.99*5) + (2.94*15) + (156.99*2) + (99.99*4) + (1.82*102):
-    raise ValueError("sum is incorrect")
-#########
