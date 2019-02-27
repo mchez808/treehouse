e32be3758e60fdb1e587b04b4e59633ef395b3a4
Mark Chesney <mark.chesney@gmail.com>
2019-02-26 22:26:40 -0800

    add more notes for web scraping course

--- advanced-python/web-scraping/Scraping Data from the Web.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen('https://treehouse-projects.github.io/horse-land/index.html')
-soup = BeautifulSoup(html.read(), 'html.parser')
-
-div = soup.find('div', {'class': 'featured'})
-print(div)
